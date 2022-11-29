6bb3fda8bf9579f5b90de5dc7d3e1746a4a80c69
Isabella Lopes <icmlopes@gmail.com>
2022-11-29 17:17:54 -0300

    fix code for deploy

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const port = process.env.PORT || 5000
 
+const port = process.env.PORT || 5000
 app.listen(port, () => 
     console.log(`Server running on port + ${port}`))
+
+// app.listen(5000, () => console.log("Server running in port : 5000"))
