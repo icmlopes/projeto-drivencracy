7e1a1697d44e65af35cd1bd5ad60b7593a1977c6
Isabella Lopes <icmlopes@gmail.com>
2022-12-01 16:19:39 -0300

    feat: get choice

--- src/controllers/choiceController.js
+++ src/controllers/choiceController.js
@@ -1,15 +1,52 @@
 import { choiceCollection } from "../database/db.js"
 
-export async function postChoice(req, res){
+export async function postChoice(req, res) {
 
-    const {title, pollId} = req.body
+    const { title, pollId } = req.body
 
-    try{
-        await choiceCollection.insertOne({title, pollId});
+    try {
+        await choiceCollection.insertOne({ title, pollId });
         res.status(201).send("Answer created successfully")
 
-    } catch (error){
+    } catch (error) {
+
         console.log(error)
         res.sendStatus(500)
     }
 }
+
+export async function getPollById(req, res) {
+    const id = req.params.id
+    console.log(id)
+
+    try {
+
+        const existingChoices = await choiceCollection.find({}).toArray()
+
+        const pollChoices = existingChoices.filter((a) => a.pollId === id)
+
+        console.log(pollChoices)
+
+        if (pollChoices.length === 0 ) {
+            res.status(404).send("Enquete não encontrada!")
+            return
+        }
+
+        res.send(pollChoices)
+
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+}
+// export async function getChoice(req, res){
+//     const id = req.params.id
+//     try{
+//         const showChoices = await choiceCollection.find(id).toArray()
+//         return res.send(showChoices)
+//     } catch(err){
+//         console.log(err)
+//         res.sendStatus(500)
+//     }
+// }
+
--- src/routes/choiceRoutes.js
+++ src/routes/choiceRoutes.js
@@ -1,10 +1,10 @@
 import {  Router } from "express"
-import { postChoice } from "../controllers/choiceController.js"
+import { getPollById, postChoice } from "../controllers/choiceController.js"
 import { choiceSchemaValidation } from "../middlewares/choiceValidationMiddleware.js"
 
 const router = Router()
 
 router.post("/choice", choiceSchemaValidation, postChoice)
-// router.get("/poll", getChoice)
+router.get("/poll/:id/choice", getPollById)
 
 export default router
